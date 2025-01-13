d73ab7970d9bf93fc08f83af6302d2cffb21b71f
mj4w <marcelaribal963@gmail.com>
2025-01-13 09:00:39 +0800

    pylint modified

--- monday_helper/graphql_queries.py
+++ monday_helper/graphql_queries.py
@@ -22,3 +22,79 @@ class GraphQLQueries:
             }}
         }}
         """
+        
+    @staticmethod
+    def get_item_details(board_id: str, 
+                         group_id: str, cursor: str) -> str:
+        """_summary_
+
+        Args:
+            board_id (str): _description_
+
+        Returns:
+            str: _description_
+        """
+        
+        return f"""
+        {{
+            boards (ids: {board_id}) {{
+                groups (ids: "{group_id}") {{
+                    id
+                    items_page (limit: 500, cursor: {f'"{cursor}"' if cursor else 'null'}) {{
+                        cursor
+                        items {{
+                            id
+                            name
+                            column_values {{
+                                ... on LocationValue {{
+                                    address
+                                    city
+                                    city_short
+                                    country
+                                    country_short
+                                    street_number
+                                    street_number_short
+                                    lat
+                                    lng
+                                    text
+                                }}
+                                ... on FileValue {{
+                                    id
+                                    files
+                                }}
+                                ... on MirrorValue {{
+                                    id
+                                    display_value
+                                }}
+                                ... on PeopleValue {{
+                                    id
+                                    text
+                                    persons_and_teams {{
+                                        id
+                                    }}
+                                }}
+                                column {{
+                                    id
+                                    title
+                                }}
+                                id
+                                text
+                                value
+                                type
+                            }}
+                            updates {{
+                                id
+                                created_at
+                                creator {{
+                                    id
+                                    name
+                                }}
+                                item_id
+                                body
+                            }}
+                        }}
+                    }}
+                }}
+            }}
+        }}
+        """
--- monday_helper/monday_api_client.py
+++ monday_helper/monday_api_client.py
@@ -1,50 +1,57 @@
-import requests
-from monday_helper.graphql_queries import GraphQLQueries
+"""
+This module contains the MondayAPIClient class to interact with the Monday.com API.
+"""
+
 from typing import Dict, Any
-
-
+from monday_helper.graphql_queries import GraphQLQueries
+import requests
 
 class MondayAPIClient:
-
+    
     """
-    A class to interact with the Monday.com API
+    A class to interact with the Monday.com API.
     """
 
     def __init__(self, api_key: str, api_url: str = "https://api.monday.com/v2"):
         """
-        Initilizes the api client in monday api
+        Initializes the API client for Monday API.
         """
         self.api_key = api_key
         self.api_url = api_url
 
     def get_headers(self) -> Dict[str, str]:
         """
-        Return headers required in monday api
-        """        
+        Returns headers required for Monday API.
+        """
         return {"Authorization": self.api_key, "API-Version": "2023-10"}
-    
+
     def make_request(self, query: str, variables: Dict[str, Any] = None) -> Dict[str, Any]:
         """
-        Makes request to the Monday Api with Graphql query
+        Makes a request to the Monday API with a GraphQL query.
         """
-
         if variables is None:
             variables = {}
 
         headers = self.get_headers()
-
-        data = {
-            'query': query,
-            'variables': variables
-        }
-
-        response = requests.post(self.api_url, json=data, headers=headers)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            raise Exception(f"Error: status-code-{response.status_code} - {response.text}")
+        response = requests.post(
+            self.api_url,
+            json={"query": query, "variables": variables},
+            headers=headers,
+            timeout=10,
+        )
+        response.raise_for_status()
+        return response.json()
 
     def get_group(self, board_id: str) -> Dict[str, Any]:
+        """
+        Queries group by ID and name.
+        """
         query = GraphQLQueries.get_board_details(board_id)
         response = self.make_request(query)
         return response
+
+    def get_items(self, board_id: str):
+        """
+        Queries items from a board.
+        """
+        query = GraphQLQueries.get_item_details(board_id)
